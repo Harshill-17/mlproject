4fd137e73d6bf248a0a370f1ade52bc397958e93
Harshill-17 <harshilsunildarji@gmail.com>
2025-08-27 14:47:50 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,43 @@
+# first import the sys library
+
+import sys
+import logging
+
+# creating my own error_message_detail that except two parametes 1. error 2. error_detail:sys in the sys.
+
+def error_message_detail(error, error_detail:sys) :
+    # so the exc_info will give you the three detail of the message so i'll need only third one so i'll write code like that
+    
+    _, _, exc_tb = error_detail.exc_info()
+    
+    # after receiving what is error, where occure, what is the line number i'll write the error_message code
+    
+    # First getting the File Name
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    
+    # Now getting the Line Number
+    line_number = exc_tb.tb_lineno
+    
+    # Now geeting the Error Message.
+    err_mess = str(error)
+    
+    error_message = "Error Occurs in Python Script name : [{0}], Line Number : [{1}], Error Message : [{2}]".format(
+        
+        file_name, line_number, err_mess
+        
+    )
+    
+    return error_message
+    
+# Now my erros raised i'll call my error_message_detail function. so creating a class CustomeException
+
+# When ever i raise my customException first it will Inherits the Parent Exception what ever error message come into the error_message_detail and it wil be intialized into the self.error_message.
+class CustomException(Exception) :
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+    
+    # if i want to print any error message
+    def __str__(self):
+        return self.error_message
+    
---
+++ src/logger.py
@@ -0,0 +1,20 @@
+# So the logger is when we execute any transaction or any other task to create the log of that class
+
+import logging
+import os
+from datetime import datetime
+
+# Creating the strucure of the Log File.
+
+# How the Log file will be saved format
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
